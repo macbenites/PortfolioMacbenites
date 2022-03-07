35929cc62dcec2991b134c1377f756bfaec616f9
macbenites <n00040723@upn.pe>
2022-03-06 23:59:07 -0500

    Add folder data

---
+++ public/data/projects.js
@@ -0,0 +1,21 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    image: "/image/react.svg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure .",
+    technologies: [
+      "React",
+      "Redux",
+      "Node",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "Jest",
+      "Enzyme",
+    ],
+    repository: "https://github.com/macbenites/pg-frontend",
+    link: "www.google.com",
+  },
+];
